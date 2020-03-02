c5787b29e75480467eaed3c9e0628ba044c06741
Markus Suomalainen <markus.suomalainen@gmail.com>
2020-03-02 23:07:02 +0200

    Run function and splitting the logic to library

--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -1,6 +1,5 @@
 use std::{env, process};
-use std::fs;
-
+use rgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,29 +11,11 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-
-}
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Self { query, filename })
+    // No need to unwrap here because Ok returns unit type () and
+    // therefore we only care about handling errors
+    if let Err(e) = rgrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
+
 }
