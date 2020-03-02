a75f98f33655871a4f13c26f573f481fe85358c4
Markus Suomalainen <markus.suomalainen@gmail.com>
2020-03-02 21:44:59 +0200

    Removed target

--- rgrep/target/debug/deps/rgrep-97bcb7fd5e615c61.d
+++
@@ -1,5 +0,0 @@
-/home/markus/Private/git/rust-testing/rgrep/target/debug/deps/rgrep-97bcb7fd5e615c61: src/main.rs
-
-/home/markus/Private/git/rust-testing/rgrep/target/debug/deps/rgrep-97bcb7fd5e615c61.d: src/main.rs
-
-src/main.rs:
--- rgrep/target/debug/rgrep.d
+++
@@ -1,1 +0,0 @@
-/home/markus/Private/git/rust-testing/rgrep/target/debug/rgrep: /home/markus/Private/git/rust-testing/rgrep/src/main.rs
