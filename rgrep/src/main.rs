cdeb9fcca8cfe64bf375e0e53c2e8f2dd95ce3f2
mureke <markus.suomalainen@gmail.com>
2020-04-18 12:35:02 +0300

    rgrep: errors to stderr

--- rgrep/src/main.rs
+++ rgrep/src/main.rs
@@ -4,7 +4,7 @@ use rgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
     // No need to unwrap here because Ok returns unit type () and
     // therefore we only care about handling errors
     if let Err(e) = rgrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
